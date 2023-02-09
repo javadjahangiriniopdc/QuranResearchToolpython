6e73062f19960b81650244b617dae5d2224150ac
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2023-02-09 15:03:05 +0330

    add Longset of aya in Qurab and Shortest of aya in Quran

--- main.py
+++ main.py
@@ -19,5 +19,19 @@ df_ayat = pd.DataFrame(quranList, columns=['aya'])
 ###
 # Count the lenght of ayayte #
 ###
-df_ayat['aye_len'] = df_ayat.aya.apply(lambda x: len(x))
-print(df_ayat)
+df_ayat['aya_len'] = df_ayat.aya.apply(lambda x: len(x))
+# print(df_ayat)
+
+###
+# Longset of aya in Qurab #
+###
+# print(max(df_ayat.aya_len))
+# print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)])
+# print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)].values)
+
+###
+# Shortest of aya in Quran #
+###
+print(min(df_ayat.aya_len))
+print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)])
+print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)].values)
