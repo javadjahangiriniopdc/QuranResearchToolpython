a1f88d40375b8aacf4b575dd10b1af7d2a3e19b2
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2023-02-13 15:46:51 +0330

    edit doc besm

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ df_ayat['aya_len'] = df_ayat.aya.apply(lambda x: len(x))
 ###
 # Longset of aya in Qurab #
 ###
-# print(max(df_ayat.aya_len))
-# print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)])
-# print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)].values)
+print(max(df_ayat.aya_len))
+print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)])
+print(df_ayat.loc[df_ayat.aya_len == max(df_ayat.aya_len)].values)
 
 ###
 # Shortest of aya in Quran #
 ###
-print(min(df_ayat.aya_len))
-print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)])
-print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)].values)
+# print(min(df_ayat.aya_len))
+# print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)])
+# print(df_ayat.loc[df_ayat.aya_len == min(df_ayat.aya_len)].values)
